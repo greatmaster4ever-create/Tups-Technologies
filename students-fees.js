dda07c3edc98eb230add1a8508efece5aa3a6288
greatmaster4ever-create <greatmaster4ever@gmail.com>
2026-06-14 06:29:58 -0700

    students-fees.js

---
+++ students-fees.js
@@ -0,0 +1,17 @@
+function showStudentsFees() {
+
+  document.getElementById(
+    "adminContent"
+  ).innerHTML = `
+
+    <h3>
+      👨‍🎓 Students & Fees
+    </h3>
+
+    <p>
+      Module Under Development
+    </p>
+
+  `;
+
+}
