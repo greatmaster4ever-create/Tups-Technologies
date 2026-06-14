d2d72192b4eed76a6deb98bb68da2910a2267d03
greatmaster4ever-create <greatmaster4ever@gmail.com>
2026-06-14 06:32:34 -0700

    payments.js

---
+++ payments.js
@@ -0,0 +1,17 @@
+function showPayments() {
+
+  document.getElementById(
+    "adminContent"
+  ).innerHTML = `
+
+    <h3>
+      💳 Payments
+    </h3>
+
+    <p>
+      Module Under Development
+    </p>
+
+  `;
+
+}
